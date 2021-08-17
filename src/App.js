05e02dee6d3f8e7c74d21a700cdddec8b7fb2c7f
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-17 06:10:46 +0500

    2.5: Search and Filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyInput from "./components/UI/input/MyInput";
 import MySelect from "./components/UI/select/MySelect";
 import "./styles/App.css";
 
@@ -10,6 +11,19 @@ function App() {
     { id: 2, title: "JavaScript 1", body: "Descrition" },
     { id: 3, title: "JavaScript 2", body: "Descrition" },
   ]);
+  
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts() {
+    console.log('Отработала функция getSortedPosts')
+    if(selectedSort) {
+      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts
+  }
+
+  const sortedPosts = getSortedPosts()
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -19,11 +33,10 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const [selectedSort, setSelectedSort] = useState('')
+
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -31,6 +44,11 @@ function App() {
       <PostForm create={createPost}/>
       <hr style={{margin: '1rem'}}/>
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Поиск..."
+        />
         <MySelect 
           value={selectedSort}
           onChange={sortPosts}
@@ -42,7 +60,7 @@ function App() {
         />
       </div>
       {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title={"JavaScript"} />
+        ? <PostList remove={removePost} posts={sortedPosts} title={"JavaScript"} />
         : <h1 style={{textAlign:'center', marginTop: '2rem'}}>Посты не найдены</h1>
       }
       
