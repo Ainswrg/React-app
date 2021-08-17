171253623befde6e8e138fcb9dd06cca3e1cc680
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-17 13:24:37 +0500

    2.9: CustomHook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React, { useMemo, useState } from "react";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyModal from "./components/UI/modal/MyModal";
-import "./styles/App.css";
+import './styles/App.css';
+
+import React, { useState } from 'react';
+
+import PostFilter from './components/PostFilter';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyModal from './components/UI/modal/MyModal';
+import { usePosts } from './hooks/usePosts';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "ff", body: "11" },
-    { id: 2, title: "aaa 1", body: "gg" },
-    { id: 3, title: "ww 2", body: "yy" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(false)
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -42,7 +27,9 @@ function App() {
 
   return (
     <div className='App'>
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>Создать пользователя</MyButton>
+      <MyButton style={{ marginTop: "30px" }} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
