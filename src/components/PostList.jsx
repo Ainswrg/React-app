cbe94db446ce13d58eb84b841642a4ad6169a5a0
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-17 05:23:23 +0500

    2.3: Remove posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PostItem from './PostItem'
 
-const PostList = ({posts, title}) => {
+const PostList = ({posts, title, remove}) => {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>{title}</h1>
-      {posts.map((post, index) => <PostItem post={post} number={index + 1} key={post.id}/>)}
+      {posts.map((post, index) => <PostItem remove={remove} post={post} number={index + 1} key={post.id}/>)}
     </div>
   )
 }
