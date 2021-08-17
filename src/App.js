159d9866cae374c9161728cbf2ef9d5cb7792db9
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-17 05:11:55 +0500

    2.2: Form Callback

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css";
 
 function App() {
@@ -11,32 +10,13 @@ function App() {
     { id: 3, title: "JavaScript 2", body: "Descrition" },
   ]);
 
-  const [post, setPost] = useState({title: '', body: ''});
-
-  const addNewPosts = (e) => {
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
-  };
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  } 
 
   return (
     <div className='App'>
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput
-          value={post.title}
-          onChange={(e) => setPost({...post, title: e.target.value})}
-          type='text'
-          placeholder='Название поста'
-        />
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({...post, body: e.target.value})}
-          type='text'
-          placeholder='Описание поста'
-        />
-        <MyButton onClick={addNewPosts}>Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title={"JavaScript"} />
     </div>
   );
