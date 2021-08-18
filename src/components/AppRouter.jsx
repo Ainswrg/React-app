7013aa63528a8e27f3e3c4d1463879756cc167f8
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-19 04:01:24 +0500

    10: Routes: public and private

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import {privateRoutes, publicRoutes} from "../router";
 
 const AppRouter = () => {
+    const isAuth = true;
     return (
-        <Switch>
-            <Route path="/about">
-                <About/>
-            </Route>
-            <Route exact path="/posts">
-                <Posts/>
-            </Route>
-            <Route exact path="/posts/:id">
-                <PostIdPage/>
-            </Route>
-            <Route path='/error'>
-                <Error />
-            </Route>
-            <Redirect to='/posts' />
-        </Switch>
+        isAuth
+            ? <Switch>
+                {privateRoutes.map(route =>
+                    <Route component={route.component} path={route.path} exact={route.exact}/>
+                )}
+                <Redirect to='/posts' />
+            </Switch>
+            : <Switch>
+                {publicRoutes.map(route =>
+                    <Route component={route.component} path={route.path} exact={route.exact}/>
+                )}
+                <Redirect to='/posts' />
+            </Switch>
+
     );
 };
 
