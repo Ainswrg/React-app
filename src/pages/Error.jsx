c5541513d2023c980f75447899674742950688ce
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-18 18:09:58 +0500

    8: Navigation.react-router-dom

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+            <h1 style={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
+                404 Страница не найдена
+            </h1>
+    );
+};
+
+export default Error;
